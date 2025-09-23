e9192753721f98a7722197332d08e25d3d2d0e37
wahyuwsap <wahyukuliah24@gmail.com>
2025-09-23 14:17:27 +0700

    latihan praktikum 3

---
+++ app/Models/UserModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = ['user'];
+    protected $guarded = ['id'];
+    public function kelas(){
+        return $this->belongsIo(Kelas::class, 'kelas_id');
+    }
+}
